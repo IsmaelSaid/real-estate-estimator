05d3847d0fb5ca685912365d0962fd4b9b971cef
SAID <ismael.saidpro@protonmail.com>
2024-07-21 22:37:31 +0400

    F5: AppBarComponent.tsx: updated the appbar with white labels and calculated height

--- src/app/components/AppBarComponent.tsx
+++ src/app/components/AppBarComponent.tsx
@@ -7,21 +7,21 @@ import {signOut, useSession} from "next-auth/react";
 export default function AppBarComponent() {
     const {data: session} = useSession()
     return (
-        <nav className={'fixed w-full z-50 top-0 left-0 bg-blue-500 flex justify-between items-center h-auto py-5 px-5'}>
+        <nav className={'fixed w-full z-50 h-16 top-0 left-0 bg-blue-500 flex justify-between items-center h-auto py-5 px-5'}>
             <div className={'text-white'}>
-                <Link className={''} href="/">HOME</Link>
+                <Link className={' text-white'} href="/">HOME</Link>
                 {session && <>
-                    <Link className={'m-2'} href="/dashboard">DASHBOARD</Link>
-                    <Link className={'m-2'} href="/prediction">PREDICTION</Link>
-                    <Link className={'m-2'} href="/contest">CONTEST</Link></>}
+                    <Link className={'m-2 text-white'} href="/dashboard">DASHBOARD</Link>
+                    <Link className={'m-2 text-white'} href="/prediction">PREDICTION</Link>
+                    <Link className={'m-2 text-white'} href="/contest">CONTEST</Link></>}
             </div>
             <div className={'text-white'}>
                 {!session && <>
-                    <Link className={'m-2'} href="/signin">SIGNIN </Link>
-                    <Link className={'m-2'} href="/signup">SIGNUP</Link></>
+                    <Link className={'m-2 text-white'} href="/signin">SIGNIN </Link>
+                    <Link className={'m-2 text-white'} href="/signup">SIGNUP</Link></>
                 }
                 {session && <div>
-                    <span className={'m-2'}>
+                    <span className={'m-2 text-white'}>
                         {session?.user?.name}
                     </span>
                     <button className={''} onClick={() => signOut({redirect: true, callbackUrl: '/'})}>SIGNOUT</button>

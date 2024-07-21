2fd74364f0ad9c4310940931a0b652bd52d3b663
SAID <ismael.saidpro@protonmail.com>
2024-07-21 22:43:51 +0400

    F5: globals.css, layout.tsx, style.css: updated globals styles with semantic ui for dropdowns

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import 'semantic-ui-css/semantic.min.css'
+import './style.css'
 import {AuthProvider} from "@/app/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
